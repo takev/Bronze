c358427bfe030b6e1b3bc7ed1155c7368c4ac640
Take Vos <take.vos@vosgames.nl>
2015-11-28 00:24:50 +0100

    Initial Commit

---
+++ Bronze/Bronze.h
@@ -0,0 +1,19 @@
+//
+//  Bronze.h
+//  Bronze
+//
+//  Created by Take Vos on 2015-11-28.
+//  Copyright © 2015 Take Vos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Bronze.
+FOUNDATION_EXPORT double BronzeVersionNumber;
+
+//! Project version string for Bronze.
+FOUNDATION_EXPORT const unsigned char BronzeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bronze/PublicHeader.h>
+
+
