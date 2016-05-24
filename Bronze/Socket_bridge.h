e6984d5ae61481e652a2e9de2d3e65c5a289d397
Take Vos <take.vos@vosgames.nl>
2016-05-24 23:39:34 +0200

    Implemented more socket calls

---
+++ Bronze/Socket_bridge.h
@@ -0,0 +1,13 @@
+//
+//  Socket_bridge.h
+//  Bronze
+//
+//  Created by Take Vos on 2016-05-21.
+//  Copyright © 2016 Take Vos. All rights reserved.
+//
+
+#ifndef Socket_bridge_h
+#define Socket_bridge_h
+
+
+#endif /* Socket_bridge_h */
---
+++ Bronze/Socket_bridge.m
@@ -0,0 +1,9 @@
+//
+//  Socket_bridge.m
+//  Bronze
+//
+//  Created by Take Vos on 2016-05-21.
+//  Copyright © 2016 Take Vos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
